d2538d8b31838ecc00f1bc3a3dea4716bf18e851
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2014-04-21 13:35:23 +0200

    Added more methods to limiter and did some refactoring

--- spec/rate_limiter/timed_limiter_spec.rb
+++ spec/rate_limiter/timed_limiter_spec.rb
@@ -8,37 +8,54 @@ describe RateLimiter::TimedLimiter do
   end
 
   it 'should be able to rate limit' do
-    limiter = subject.new('foobar', period: 10, rate: 5)
     counter = 0
 
     current_time = 10
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(limiter.limit(current_time) { counter += 1 }).to eq false
-    expect(counter).to eq 5
+    limiter = subject.new('foobar', interval: 10, rate: 2, clock: -> { current_time })
+
+    expect(limiter.limit { counter += 1 }).to eq true
+    expect(limiter.limit { counter += 1 }).to eq true
+    expect(limiter.limit { counter += 1 }).to eq false
+    expect(counter).to eq 2
   end
 
-  it 'should be able to reset after a time period' do
-    limiter = subject.new('foobar', period: 10, rate: 5)
+  it 'should be able to reset after a time interval' do
     counter = 0
 
     current_time = 10
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(counter).to eq 5
+    limiter = subject.new('foobar', interval: 10, rate: 2, clock: -> { current_time })
+
+    expect(limiter.limit { counter += 1 }).to eq true
+    expect(limiter.limit { counter += 1 }).to eq true
+    expect(counter).to eq 2
 
     current_time = 20
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(limiter.limit(current_time) { counter += 1 }).to eq true
-    expect(counter).to eq 10
+    expect(limiter.limit { counter += 1 }).to eq true
+    expect(limiter.limit { counter += 1 }).to eq true
+    expect(counter).to eq 4
+  end
+
+  it 'should know when limit is exceeded' do
+    counter = 0
+
+    current_time = 10
+    limiter = subject.new('foobar', interval: 10, rate: 2, clock: -> { current_time })
+
+    expect(limiter.limit { counter += 1 }).to eq true
+    expect(limiter.limit { counter += 1 }).to eq true
+    expect(counter).to eq 2
+
+    expect(limiter.exceeded?).to eq true
+  end
+
+  it 'should be able to increment on its own' do
+    current_time = 10
+    limiter = subject.new('foobar', interval: 10, rate: 2, clock: -> { current_time })
+
+    expect(limiter.increment!).to eq true
+    expect(limiter.increment!).to eq true
+    expect(limiter.exceeded?).to eq true
+
+    expect(limiter.increment!).to eq false
   end
 end
